f7f807760ec11643060e830e30f3bf6dd9e718c4
Fanny <renatenyatsikor@gmail.com>
2024-02-06 16:17:27 +0100

    generate the metadataFile

--- languageDetectionApp/main.py
+++ languageDetectionApp/main.py
@@ -17,17 +17,14 @@ logging.basicConfig(level=logging.INFO)
 #Metadata file formant
 metadata ={}
 #RabbitMQ connection
-@retry(delay=5, backoff=2, max_delay=60, logger=None)
+@retry(delay=5, backoff=2, max_delay=60,logger=None)
 def connect_to_rabbitmq():
     connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
     return connection
 
-
-
 def extract_audio(video_name):
     input_video = "data/" + video_name
     input_video_name = input_video.split("/")[-1].replace(".mp4", "") # remove .mp4
-
     extracted_audio = f"audio-{input_video_name}.wav"
     stream = ffmpeg.input(input_video)
     stream = ffmpeg.output(stream, extracted_audio)
@@ -65,37 +62,45 @@ def generate_subtitles(segments):
         text +=f"{segment.text}"
         substite_file.append(text)
         text = ""
-
     return substite_file
 
+def generate_metadata(video_name, subtitles, language):
+    metadataDico = ffmpeg.probe("data/" + video_name)
+    metadata = {
+        "video_path": "data/" + video_name,
+        "duration": metadataDico["format"]["duration"],
+        "bit_rate": metadataDico["format"]["bit_rate"],
+        "size": metadataDico["format"]["size"],
+        "nb_streams": metadataDico["format"]["nb_streams"],
+        "first_stream_codec_name": metadataDico["streams"][0]["codec_name"],
+        "second_stream_codec_name": metadataDico["streams"][1]["codec_name"],  # ???? 
+        "subtitles": subtitles,
+        "language": language
+    }
 
-
-def run(video_name):
-   
-    extracted_audio = extract_audio(video_name)
-    language, segments = transcribe_audio(audio=extracted_audio)
-    subtitles = generate_subtitles(segments)
-    logging.info(f"Subtitles: {subtitles}")
-    logging.info(f"Language detected: {language}")
-
-    metadata = ffmpeg.probe("data/" + video_name)
     logging.info(f"Metadata: {metadata}")
-    metadata["subtitles"] = subtitles
-    metadata["language"] = language
     json_metadata_file_path = f"data/Metadata_{video_name.replace('.mp4', '.json')}"
     with open(json_metadata_file_path, 'w') as json_file:
         json.dump(metadata, json_file)
     logging.info(f"Metadata file: {json_metadata_file_path}")
     return json_metadata_file_path.split("/")[-1]
 
+
+def run(video_name):
+    extracted_audio = extract_audio(video_name)
+    language, segments = transcribe_audio(audio=extracted_audio)
+    subtitles = generate_subtitles(segments)
+    logging.info(f"Subtitles: {subtitles}")
+    logging.info(f"Language detected: {language}")
+    metadatafile = generate_metadata(video_name, subtitles, language)
+    return metadatafile
+    
+
 def main():
 
-    logging.info("########################START################################")
-    
+    logging.info("########################GET LANGUAGE AND SUBSTITLES POD ################################")
     connection = connect_to_rabbitmq()
     channel = connection.channel()
-
-    video_name = ""
     # create a queue to receive the video name from the downscale app
     channel.queue_declare(queue='donwscale_process')  
     channel.queue_declare(queue='detect_animals_process')
@@ -113,9 +118,6 @@ def main():
     channel.basic_consume(queue='donwscale_process', on_message_callback=callback, auto_ack=True)
     channel.start_consuming()
     
-    # connection.close()
-
-
 if __name__ == "__main__":
     
     main()
